a3dd1edda9755b7958679d062f2a3b5f681b9f88
Romain Testard <romain.testard@bluenove.com>
2021-01-17 21:44:49 +0100

    I have made heroes route public

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .serializers import HeroSerializer
 from .models import Hero
@@ -7,3 +7,4 @@ from .models import Hero
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
+    permission_classes = (permissions.AllowAny,)
